1c59ff66461b361f53c502453172181ed5d01143
Renu1827 <87928833+Renu1827@users.noreply.github.com>
2021-08-16 19:32:06 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+function setup() { 
+    createCanvas(400,400);
+  }
+  
+  function draw() 
+  {
+    background(30);
+  
+  }
+  
+  function draw() {
+  
+    if (keyIsDown(DOWN_ARROW))
+    {
+      background("green");
+    }
+  
+    if (keyIsDown(UP_ARROW))
+    {
+      background(" purple");
+    }
+  
+    if (keyIsDown(LEFT_ARROW))
+    {
+      background("pink");
+    }
+  
+    if (keyIsDown(RIGHT_ARROW))
+    {
+      background("yellow");
+    }
+  
+  
+    
+   
+  }
+  
+
+
+
